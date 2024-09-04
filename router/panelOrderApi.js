dc2452f2df3a092a277c994942b63bafb1030a3d
milad <milad.cia@gmail.com>
2024-09-04 14:06:45 +0330

    user

--- router/panelOrderApi.js
+++ router/panelOrderApi.js
@@ -127,8 +127,7 @@ router.post('/list',jsonParser,async (req,res)=>{
             var contractorInfo =''
             if(orderList[i].contractor)
                 contractorInfo= await UserSchema.findOne({cCode:orderList.contractor})
-            else
-                contractorInfo=[{}]
+            
             orderList[i].contractorInfo = contractorInfo 
         }
 
